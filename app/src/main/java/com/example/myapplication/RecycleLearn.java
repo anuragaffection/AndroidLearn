472132a18797ee97cea5aba0b6ea2ec2c154282d
Anurag Affection <anuragaffection07@gmail.com>
2023-02-24 14:02:30 +0530

    update

---
+++ app/src/main/java/com/example/myapplication/RecycleLearn.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecycleLearn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_learn);
+    }
+}
